d751f1dda9a1a5d15c4bd3c8001f626df172ef2e
Jack Lingle <captainjack0818@gmail.com>
2023-03-02 13:21:40 -0500

    Add array splicing exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(arr, rmFromArray) {
+    let args = Array.from(arguments).splice(1);
+    for (let i = 0; i < args.length; i++) {
+        let index = arr.indexOf(args[i]);
+        if (index !== -1) {
+            arr.splice(index, 1);
+        };
+    };
+    return arr;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
