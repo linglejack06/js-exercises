1c119ffe8e898cc83f82dcf83f1e77582e4805e8
Jack Lingle <captainjack0818@gmail.com>
2023-03-02 12:00:19 -0500

    Add Repeat String Exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    let repeated = ''
+    for (num > 0; num--;) {
+        repeated += word;
+    }
+    return repeated;
 };
 
 // Do not edit below this line
