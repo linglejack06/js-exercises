73b93eb96f788c59ec9b367a55e36dfa09dcfe86
Jack Lingle <captainjack0818@gmail.com>
2023-03-03 11:58:27 -0500

    Add looping number exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, finish) {
+    if(start < 0 || finish < 0) {
+        return 'ERROR';
+    }
+    if (!Number.isInteger(start) || !Number.isInteger(finish)) {
+        return 'ERROR';
+    }
+    if (start > finish) {
+        let temp = start;
+        start = finish;
+        finish = temp;
+    }
+    let sum = 0;
+    for (let i = start; i < finish + 1; i++) {
+        sum += i;
+    };
+    return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
