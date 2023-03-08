1ce176081478393d31caee3852c3d78bb972c3b9
Jack Lingle <captainjack0818@gmail.com>
2023-03-08 12:11:54 -0500

    Add calculator solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return a.reduce((accumulator, currentValue) => accumulator + currentValue, 0,);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+    return a.reduce((total, current) => total * current);
 };
 
-const power = function() {
-	
+const power = function(num, exponent) {
+  let total = num;
+	for(i = 0; i < exponent - 1; i++) {
+    total *= num;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) return 1;
+  return n * factorial(n-1);
 };
 
 // Do not edit below this line
