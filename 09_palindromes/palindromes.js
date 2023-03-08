299658fef3d202065a1594484751edceff263da6
Jack Lingle <captainjack0818@gmail.com>
2023-03-08 12:29:28 -0500

    Add palindrome solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    let simplePhrase = phrase.toLowerCase().replace(/\W/g, '');
+    let strArray = simplePhrase.split('').reverse();
+    let reversePhrase = strArray.join('');
+    return reversePhrase === simplePhrase;
 
 };
-
+console.log(palindromes("Hi my name is Jack. I'm cool."));
 // Do not edit below this line
 module.exports = palindromes;
